45d49911206d2b76d1f469a4efd97f163fdd2d57
Leenrow <louys.martin07@gmail.com>
2024-12-04 10:09:14 +0100

    pages

---
+++ src/app/pages/menu-catalogue/menu-catalogue.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-catalogue',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-catalogue.component.html',
+  styleUrl: './menu-catalogue.component.css'
+})
+export class MenuCatalogueComponent {
+
+}
