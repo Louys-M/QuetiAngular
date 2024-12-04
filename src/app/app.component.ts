69948e5814e077daa01d68c4f477341a423bdfa0
haybeeden <162558115+haybeeden@users.noreply.github.com>
2024-12-04 08:37:36 +0100

    app header + footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component'
+import { FooterComponent } from './footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent
-  ],
+  imports: [RouterOutlet, MainComponent,HeaderComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
