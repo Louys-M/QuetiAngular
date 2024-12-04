d9f56690cc085ea3e12add5dff4c5816bf7fac5f
Leenrow <louys.martin07@gmail.com>
2024-12-04 11:44:37 +0100

    header route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { CatalogueComponent } from './pages/catalogue/catalogue.component';
+import { DescriptionComponent } from './pages/description/description.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MenuCatalogueComponent } from './pages/menu-catalogue/menu-catalogue.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ResultatComponent } from './pages/resultat/resultat.component';
+import { InsecteComponent } from './Queti/insecte/insecte.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component:AccueilComponent},
+    {path: 'login', component:LoginComponent},
+    {path: 'register', component:RegisterComponent},
+    {path: 'resultat', component:ResultatComponent},
+    {path: 'ordre', component:MenuCatalogueComponent},
+    {path: 'ordre/:ordreID', component:CatalogueComponent},
+    {path: 'insecte/:insecteID', component:InsecteComponent},
+    {path: 'parametres', component:SettingsComponent},
+    {path: '**', component:AccueilComponent},
+
+];
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Route, Router, RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+// import { AuthService } from '../Services/auth.service';
+// import { ChangeDetectorRef } from '@angular/core';  
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterOutlet, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
-}
+  // //ajouter un détecteur de changement qui permettra d'actualiser le component si changement
+  // private cd!: ChangeDetectorRef // Injecter le ChangeDetectorRef
+  // //on a besoin du service d'authentification pour savoir si on est connecté ou pas
+  // constructor(protected authService : AuthService, public router:Router) { }
+  // logout():void{
+  // this.authService.logout();
+  // this.router.navigate(['/login']);
+  // }
+  }
