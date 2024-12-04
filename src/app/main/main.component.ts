958dc3d9ea3e54ff0ba96283e0d73c86d4ca05d5
haybeeden <162558115+haybeeden@users.noreply.github.com>
2024-12-04 09:39:05 +0100

    insect comp avec donnees statiques et affichage

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { InsecteComponent } from '../Queti/insecte/insecte.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [InsecteComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
