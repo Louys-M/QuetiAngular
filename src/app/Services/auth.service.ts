da82655d4a8c2b51a4ed78bdd30831514c64705e
haybeeden <162558115+haybeeden@users.noreply.github.com>
2024-12-09 09:30:46 +0100

    connexion

---
+++ src/app/Services/auth.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private router: Router) { }
+  url = environment.api_url + '/login';
+  token: string | null = null;
+  is_connected = false;
+  error: string = '';
+  //utilisation d’un BehaviorSubject pour créer une reaction lors du changement de valeur sur d’autres components
+  private loggedIn = new BehaviorSubject<boolean>(this.isAuthenticated());
+  async login(email: string, password: string) {
+    const response = await fetch(this.url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password })
+    });
+    const data = await response.json();
+    console.log(data.message);
+
+    this.error = data.message;
+    if (response.ok) {
+      this.token = data.access_token;
+      console.log(this.token);
+      this.is_connected = true;
+      if (this.token != null) {
+        //enregistrement du token dans le localStorage
+        localStorage.setItem('authToken', this.token);
+        //mise à jour du BehaviorSubject, pour pouvoir faire réagir les autres component au changement
+        this.loggedIn.next(true); // Met à jour l'état après connexion
+        console.log('connexion réussie');
+        //rediection vers la page d'accueil quand connecté
+        this.router.navigate(['/home']);
+      };
+    }
+    else {
+      this.error = 'connexion impossible'; /// connexion impossible, et pourtant connecté...
+      console.log(this.error);
+    }
+  }
+  getToken(): string | null {
+    return localStorage.getItem('authToken');
+  }
+  logout() {
+    this.token = null;
+    this.is_connected = false;
+    console.log('deconnexion reussie');
+    //on efface de token lors de la déconnexion
+    localStorage.removeItem('authToken');
+    //on met à jour la valeur du BehaviorSubject
+    this.loggedIn.next(false);
+    //Redirection vers la page de login
+    this.router.navigate(['login']);
+  }
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem('authToken');
+  }
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgForm, FormsModule } from '@angular/forms';
 import { EmailValidator } from '@angular/forms';
+import { AuthService } from '../../Services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,16 @@ import { EmailValidator } from '@angular/forms';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  onSubmit(form: NgForm){
-    console.log(form.value);
-    }
+  errorMessage:string = '';
+  constructor(private authService : AuthService, private router : Router) { }
+  async onSubmit(form: NgForm){
+  console.log(form.value);
+  try {
+  await this.authService.login(form.value.email, form.value.password);
+  console.log('connecté');
+  this.router.navigate(['/home']); // Rediriger vers le tableau de bord après la connexion réussie
+  } catch (error:any) {
+  console.log("Message : "+error);
+  }
+  }
 }
