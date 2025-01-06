26b553e29b5a76ce3b2e49e38ba8ebf40908abfa
Leenrow <louys.martin07@gmail.com>
2025-01-06 13:56:46 +0100

    casstou

--- src/app/Queti/insecte/insecte.component.ts
+++ src/app/Queti/insecte/insecte.component.ts
@@ -1,21 +1,59 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { Insecte } from '../../models/insecte';
+// import { Component, OnInit,Input } from '@angular/core';
+// import { Insecte } from '../../models/insecte';
+// import { RouterLinkActive } from '@angular/router';
+// import { Route, Router, RouterLink } from '@angular/router';
+// import { AuthService } from '../../Services/auth.service';
 
-@Component({
-  selector: 'app-insecte',
-  standalone: true,
-  imports: [],
-  templateUrl: './insecte.component.html',
-  styleUrl: './insecte.component.css'
-})
-export class InsecteComponent {
+// @Component({
+//   selector: 'app-insecte',
+//   standalone: true,
+//   imports: [RouterLink, RouterLinkActive],
+//   templateUrl: './insecte.component.html',
+//   styleUrl: './insecte.component.css'
+// })
+// export class InsecteComponent {
 
-  @Input() insecte!: Insecte;
+//   likedInsects: Set<number> = new Set(); // Store liked insect IDs
 
-  id! : number;
-  nom_sc! : string;
-  nom_fr!: string;
-  photo!: string;
-  description!: string;
+//   constructor(protected authService: AuthService) { }
 
-}
+//   @Input() insecte!: Insecte;
+//   @Input() insectId!: number;
+//   isLiked = false;
+
+//   ngOnInit() {
+//     this.authService.getUserLikedInsect().subscribe((likedInsects) => {
+//       this.likedInsects = likedInsects;
+//       this.isLiked = likedInsects.has(this.insectId);
+//     });
+//   }
+
+//   toggleLike(insectId: number): void {
+//     if (this.likedInsects.has(insectId)) {
+//       // Unlike the insect
+//       this.authService.userUnlikeInsect(insectId).subscribe({
+//         next: () => {
+//           this.likedInsects.delete(insectId); // Update local state
+//           console.log(Insect ${insectId} unliked successfully.);
+//         },
+//         error: (err) => {
+//           console.error(Failed to unlike insect ${insectId}:, err);
+//         },
+//       });
+//     } else {
+//       // Like the insect
+//       this.authService.userLikeInsect(insectId).subscribe({
+//         next: () => {
+//           this.likedInsects.add(insectId); // Update local state
+//           console.log(Insect ${insectId} liked successfully.);
+//         },
+//         error: (err) => {
+//           console.error(Failed to like insect ${insectId}:, err);
+//         },
+//       });
+//     }
+//   }
+
+
+
+// }
